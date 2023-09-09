ee7bae2442b894bb93a55b46141a88fafc4d948e
Alessandro Valitutti <metavivo@users.noreply.github.com>
2023-09-09 15:29:06 +0200

    Create step7.js

---
+++ kinetic-typography/2_text-to-points/step7.js
@@ -0,0 +1,73 @@
+       /*
+            3. use opentype.js for more control
+            reference: https://github.com/opentypejs/opentype.js
+        */
+
+        let font // opentype.js font object
+        let msg // text to write
+        let pts = [] // store path data
+        let path
+        
+        let fSize = 40
+        let sw = 4
+        let d = 0
+
+        function setup() {
+            createCanvas(800, 500)
+
+//            opentype.load('Inconsolata.otf', function (err, f) {
+            opentype.load('Zap.ttf', function (err, f) {
+//            opentype.load('arial.ttf', function (err, f) {
+                if (err) {
+                    alert('Font could not be loaded: ' + err);
+                } else {
+                    font = f
+                    console.log('font ready')
+
+//                    fSize = 40
+                    msg = 'Ciaooo!'
+
+                    let x = 60;
+                    let y = 300;
+                    path = font.getPath(msg, x, y, fSize);
+                    console.log(path.commands)
+                }
+            })
+        }
+
+        function draw() {
+          
+          
+          
+          
+            if (!font) return
+
+//            background(0)
+            background(255)
+            noFill()
+//            stroke(255)
+            stroke(0)
+//          strokeWeight(4)
+          strokeWeight(sw)
+//            fill(0)  
+          
+            for (let cmd of path.commands) {
+
+                cmd.x = cmd.x + d;
+                cmd.x1 = cmd.x1 + d;
+                cmd.x2 = cmd.x2 + d;
+
+              if (cmd.type === 'M') {
+                    beginShape()
+                    vertex(cmd.x, cmd.y)
+                } else if (cmd.type === 'L') {
+                    vertex(cmd.x, cmd.y)
+                } else if (cmd.type === 'C') {
+                    bezierVertex(cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y)
+                } else if (cmd.type === 'Q') {
+                    quadraticVertex(cmd.x1, cmd.y1, cmd.x, cmd.y)
+                } else if (cmd.type === 'Z') {
+                    endShape(CLOSE)
+                }
+            }
+        }
