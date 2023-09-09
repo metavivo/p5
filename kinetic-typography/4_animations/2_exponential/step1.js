413dab7c2bb9da00007ec0ef6bc0742ef458e6f2
Alessandro Valitutti <metavivo@users.noreply.github.com>
2023-09-09 17:12:29 +0200

    Create step1.js

---
+++ kinetic-typography/4_animations/2_exponential/step1.js
@@ -0,0 +1,21 @@
+let x = 0
+let v = -0.1
+let a = 0.3
+
+function setup() {
+  createCanvas(600, 400)
+  
+  rect(x,100,100,100)
+  
+  
+}
+
+function draw() {
+  background(220)
+
+  v = v + a
+  x = x + v
+  
+  rect(x,100,100,100)
+
+}
