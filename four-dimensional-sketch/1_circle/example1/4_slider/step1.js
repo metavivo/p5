215ad647c789664b4525f5feaf81b0a38b9410f8
Alessandro Valitutti <metavivo@users.noreply.github.com>
2023-09-13 11:28:00 +0200

    Update step1.js

--- four-dimensional-sketch/1_circle/example1/4_slider/step1.js
+++ four-dimensional-sketch/1_circle/example1/4_slider/step1.js
@@ -1,1 +1,16 @@
-sdfa
+let r = 100;
+let slider;
+
+function setup() {
+  createCanvas(600, 400);
+
+  slider = createSlider(0, 200, 100);
+
+}
+
+function draw() {
+  background(220);
+  
+  let r = slider.value();
+  circle(width/2,height/2,r);
+}
