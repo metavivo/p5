8aea71b2f6429a79f13dc57bfc4ac5195ab710ce
Alessandro Valitutti <metavivo@users.noreply.github.com>
2023-09-13 10:24:44 +0200

    Update step1.js

--- simulazioni-fisica/2_fisica-seconde/1_ottica-geometrica/raggio-ombra/2_punto/disegno/example1/step1.js
+++ simulazioni-fisica/2_fisica-seconde/1_ottica-geometrica/raggio-ombra/2_punto/disegno/example1/step1.js
@@ -1,1 +1,26 @@
-sdfas
+function setup() {
+  createCanvas(600, 400);
+  background(220);
+  
+  strokeWeight(12)
+  stroke(255,0,0)
+  point(100,100)
+  strokeWeight(1)
+  
+  
+  circle(200,200,20)
+  
+  fill(0,255,0)
+  circle(300,200,20)
+  
+  fill(0,0,255)
+  stroke(0,0,255)
+  circle(400,200,20)
+  
+  ellipse(500,300, 50,80)
+  
+  rect(100, 300, 50, 80)
+  
+  square(100,200,40)
+  
+}
